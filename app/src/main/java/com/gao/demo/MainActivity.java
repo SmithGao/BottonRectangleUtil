a3cc835f2c468ca4d6c1c17d836e5d047e524095
SimthGao <2215719882@qq.com>
2016-03-29 23:04:31 +0800

    //111

--- app/src/main/java/com/gao/demo/MainActivity.java
+++ app/src/main/java/com/gao/demo/MainActivity.java
@@ -2,7 +2,6 @@ package com.gao.demo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +9,5 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
 	}
 }
